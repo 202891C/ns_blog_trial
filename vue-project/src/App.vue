f624f2997c81507a8613bd6cba60c8b72ce4a4ea
202891C <202891C@mymail.nyp.edu.sg>
2023-07-31 10:01:00 +0800

    Comment out Quick Start default Vue codes
    Page will be blank. Removal of codes will be done at a later stage.

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,10 +1,10 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+// import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
-  <header>
+  <!-- <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
@@ -14,17 +14,17 @@ import TheWelcome from './components/TheWelcome.vue'
 
   <main>
     <TheWelcome />
-  </main>
+  </main> -->
 </template>
 
-<style scoped>
+<!-- <style scoped>
 header {
   line-height: 1.5;
 }
 
 .logo {
   display: block;
-  margin: 0 auto 2rem;
+  margin: 0 auto 2rem; /*comment*/
 }
 
 @media (min-width: 1024px) {
@@ -44,4 +44,4 @@ header {
     flex-wrap: wrap;
   }
 }
-</style>
+</style> -->
