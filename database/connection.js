a5cfbc3a4532aa215f4fc19192363d006f3de125
202891C <202891C@mymail.nyp.edu.sg>
2023-07-31 16:51:53 +0800

    Connect Fastify to Database

---
+++ database/connection.js
@@ -0,0 +1,9 @@
+// const mysql = require('mysql2');
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     database: 'ns_trial',
+//     multipleStatements: true
+// });
+
+// module.exports = { connection };
--- fastify/server.js
+++ fastify/server.js
@@ -1,13 +1,44 @@
+// Require the framework and instantiate it
 const fastify = require('fastify')({ logger: true })
 
-fastify.get('/', function handler (request, reply) {
+{/* <script>const cors = require("cors");</script> */}
+
+fastify.register(require('@fastify/mysql'), {
+  connectionString: 'mysql://root@localhost/ns_trial'
+})
+
+// Declare a route
+fastify.get('/routes', function handler (request, reply) {
     // reply.redirect("https://fastify.dev/")
     reply.send({ hello: 'world' })
-  })
+})
 
+fastify.get('/blog/:id', function(req, reply) {
+    fastify.mysql.query(
+      'SELECT Id, Title, User, Date, Description FROM blogs WHERE id=?', [req.params.id],
+      function onResult (err, result) {
+        reply.send(err || result)
+      }
+    )
+})
+
+fastify.get('/blog', function(req, reply) {
+  fastify.mysql.query(
+    'SELECT Id, Title, User, Date, Description FROM blogs',
+    function onResult (err, result) {
+      reply.send(err || result)
+    }
+  )
+})
+
+// Run the server!
 fastify.listen({ port: 3000 }, (err) => {
     if (err) {
       fastify.log.error(err)
       process.exit(1)
     }
-  })
+})
+
+// "npm install fastify"
+// 'Using CJS codes, launch with "node server" and test on curl "http://localhost:3000"'
+// https://fastify.dev/
